9c895ed7d38d3ad30852846d3eb94cd16661cf3f
雨心 <i@mezzp.com>
2019-03-05 21:28:40 +0800

    txt文件版本

---
+++ 项目1-用户登录程序/02登录-文件版/Login/Login/FileOption.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class FileOption
+    {
+        static string path = @"../../users.txt";
+        /// <summary>
+        /// 把用户信息写入文件
+        /// </summary>
+        /// <param name="line">要写入的用户信息</param>
+        /// <returns>true:写入成功；false：写入失败</returns>
+        static public bool writerFile(Users u)
+        {
+            bool result = false;
+            string line = u.UserName + "\t" + u.Password;
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(path, true, Encoding.GetEncoding("utf-8"));
+                sw.WriteLine(line);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sw.Close();
+            }
+            return result;
+
+        }
+        /// <summary>
+        /// 去文件读取用户
+        /// </summary>
+        /// <param name="u">要读取的用户信息</param>
+        /// <returns>true:读取成功；false：读取失败</returns>
+        static public bool readFile(Users u)
+        {
+            bool result = false;
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(path, Encoding.GetEncoding("utf-8"));
+                string[] str;
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    str = line.Split('\t');
+                    if (str[0] == u.UserName && str[1] == u.Password)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sr.Close();
+            }
+            return result;
+        }
+        static public bool readFile(string userName)
+        {
+            bool result = false;
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(path, Encoding.GetEncoding("utf-8"));
+                string[] str;
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    str = line.Split('\t');
+                    if (str[0] == userName)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sr.Close();
+            }
+            return result;
+        }
+    }
+}
+
---
+++ 项目1-用户登录程序/02登录-文件版/Login/Login/Register.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Login
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword2 = new System.Windows.Forms.Label();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(217, 224);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 34);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "清空";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(85, 224);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(112, 34);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "注册";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(193, 103);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(148, 28);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(85, 106);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(98, 18);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "密    码：";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(193, 54);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(148, 28);
+            this.txtUser.TabIndex = 1;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(85, 57);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(98, 18);
+            this.lblUser.TabIndex = 6;
+            this.lblUser.Text = "用 户 名：";
+            // 
+            // lblPassword2
+            // 
+            this.lblPassword2.AutoSize = true;
+            this.lblPassword2.Location = new System.Drawing.Point(87, 158);
+            this.lblPassword2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword2.Name = "lblPassword2";
+            this.lblPassword2.Size = new System.Drawing.Size(98, 18);
+            this.lblPassword2.TabIndex = 5;
+            this.lblPassword2.Text = "确认密码：";
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.Location = new System.Drawing.Point(193, 155);
+            this.txtPassword2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.PasswordChar = '*';
+            this.txtPassword2.Size = new System.Drawing.Size(148, 28);
+            this.txtPassword2.TabIndex = 3;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 318);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtPassword2);
+            this.Controls.Add(this.lblPassword2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "注册";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPassword2;
+        private System.Windows.Forms.TextBox txtPassword2;
+    }
+}
