65ab65ad19d00967de4fa9b3e136099df6d11b9d
雨心 <i@mezzp.com>
2019-03-27 10:48:55 +0800

    项目3-客户问卷调查程序开始

---
+++ 项目3-客户问卷调查程序/简单版/Questionnaire/Questionnaire/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Questionnaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion2 = new System.Windows.Forms.Label();
+            this.lblQuestion3 = new System.Windows.Forms.Label();
+            this.lblQuestion4 = new System.Windows.Forms.Label();
+            this.lblQuestion5 = new System.Windows.Forms.Label();
+            this.lblQuestion1 = new System.Windows.Forms.Label();
+            this.rbtnAnswer1 = new System.Windows.Forms.RadioButton();
+            this.rbtnAnswer2 = new System.Windows.Forms.RadioButton();
+            this.chkAnswer1 = new System.Windows.Forms.CheckBox();
+            this.chkAnswer2 = new System.Windows.Forms.CheckBox();
+            this.chkAnswer3 = new System.Windows.Forms.CheckBox();
+            this.chkAnswer4 = new System.Windows.Forms.CheckBox();
+            this.lstAnswer = new System.Windows.Forms.ListBox();
+            this.cboAnwer = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblQuestion2
+            // 
+            this.lblQuestion2.AutoSize = true;
+            this.lblQuestion2.Location = new System.Drawing.Point(60, 95);
+            this.lblQuestion2.Name = "lblQuestion2";
+            this.lblQuestion2.Size = new System.Drawing.Size(77, 12);
+            this.lblQuestion2.TabIndex = 0;
+            this.lblQuestion2.Text = "2.您的性别：";
+            // 
+            // lblQuestion3
+            // 
+            this.lblQuestion3.AutoSize = true;
+            this.lblQuestion3.Location = new System.Drawing.Point(60, 152);
+            this.lblQuestion3.Name = "lblQuestion3";
+            this.lblQuestion3.Size = new System.Drawing.Size(185, 12);
+            this.lblQuestion3.TabIndex = 0;
+            this.lblQuestion3.Text = "3.您对本公司业务员的总体印象：";
+            // 
+            // lblQuestion4
+            // 
+            this.lblQuestion4.AutoSize = true;
+            this.lblQuestion4.Location = new System.Drawing.Point(60, 234);
+            this.lblQuestion4.Name = "lblQuestion4";
+            this.lblQuestion4.Size = new System.Drawing.Size(185, 12);
+            this.lblQuestion4.TabIndex = 0;
+            this.lblQuestion4.Text = "4.您最需要本公司提供的服务是：";
+            // 
+            // lblQuestion5
+            // 
+            this.lblQuestion5.AutoSize = true;
+            this.lblQuestion5.Location = new System.Drawing.Point(298, 234);
+            this.lblQuestion5.Name = "lblQuestion5";
+            this.lblQuestion5.Size = new System.Drawing.Size(185, 12);
+            this.lblQuestion5.TabIndex = 0;
+            this.lblQuestion5.Text = "5:您对我们公司最满意的方面是：";
+            // 
+            // lblQuestion1
+            // 
+            this.lblQuestion1.AutoSize = true;
+            this.lblQuestion1.Location = new System.Drawing.Point(60, 44);
+            this.lblQuestion1.Name = "lblQuestion1";
+            this.lblQuestion1.Size = new System.Drawing.Size(77, 12);
+            this.lblQuestion1.TabIndex = 1;
+            this.lblQuestion1.Text = "1:您的姓名：";
+            // 
+            // rbtnAnswer1
+            // 
+            this.rbtnAnswer1.AutoSize = true;
+            this.rbtnAnswer1.Location = new System.Drawing.Point(144, 95);
+            this.rbtnAnswer1.Name = "rbtnAnswer1";
+            this.rbtnAnswer1.Size = new System.Drawing.Size(35, 16);
+            this.rbtnAnswer1.TabIndex = 2;
+            this.rbtnAnswer1.TabStop = true;
+            this.rbtnAnswer1.Text = "男";
+            this.rbtnAnswer1.UseVisualStyleBackColor = true;
+            // 
+            // rbtnAnswer2
+            // 
+            this.rbtnAnswer2.AutoSize = true;
+            this.rbtnAnswer2.Location = new System.Drawing.Point(210, 95);
+            this.rbtnAnswer2.Name = "rbtnAnswer2";
+            this.rbtnAnswer2.Size = new System.Drawing.Size(35, 16);
+            this.rbtnAnswer2.TabIndex = 3;
+            this.rbtnAnswer2.TabStop = true;
+            this.rbtnAnswer2.Text = "女";
+            this.rbtnAnswer2.UseVisualStyleBackColor = true;
+            // 
+            // chkAnswer1
+            // 
+            this.chkAnswer1.AutoSize = true;
+            this.chkAnswer1.Location = new System.Drawing.Point(71, 192);
+            this.chkAnswer1.Name = "chkAnswer1";
+            this.chkAnswer1.Size = new System.Drawing.Size(48, 16);
+            this.chkAnswer1.TabIndex = 4;
+            this.chkAnswer1.Text = "诚信";
+            this.chkAnswer1.UseVisualStyleBackColor = true;
+            // 
+            // chkAnswer2
+            // 
+            this.chkAnswer2.AutoSize = true;
+            this.chkAnswer2.Location = new System.Drawing.Point(155, 192);
+            this.chkAnswer2.Name = "chkAnswer2";
+            this.chkAnswer2.Size = new System.Drawing.Size(48, 16);
+            this.chkAnswer2.TabIndex = 5;
+            this.chkAnswer2.Text = "耐心";
+            this.chkAnswer2.UseVisualStyleBackColor = true;
+            // 
+            // chkAnswer3
+            // 
+            this.chkAnswer3.AutoSize = true;
+            this.chkAnswer3.Location = new System.Drawing.Point(256, 192);
+            this.chkAnswer3.Name = "chkAnswer3";
+            this.chkAnswer3.Size = new System.Drawing.Size(48, 16);
+            this.chkAnswer3.TabIndex = 6;
+            this.chkAnswer3.Text = "懒散";
+            this.chkAnswer3.UseVisualStyleBackColor = true;
+            // 
+            // chkAnswer4
+            // 
+            this.chkAnswer4.AutoSize = true;
+            this.chkAnswer4.Location = new System.Drawing.Point(367, 192);
+            this.chkAnswer4.Name = "chkAnswer4";
+            this.chkAnswer4.Size = new System.Drawing.Size(48, 16);
+            this.chkAnswer4.TabIndex = 7;
+            this.chkAnswer4.Text = "拖沓";
+            this.chkAnswer4.UseVisualStyleBackColor = true;
+            // 
+            // lstAnswer
+            // 
+            this.lstAnswer.FormattingEnabled = true;
+            this.lstAnswer.ItemHeight = 12;
+            this.lstAnswer.Items.AddRange(new object[] {
+            "售前咨询",
+            "业务接洽",
+            "售后培训",
+            "售后理赔"});
+            this.lstAnswer.Location = new System.Drawing.Point(71, 262);
+            this.lstAnswer.Name = "lstAnswer";
+            this.lstAnswer.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstAnswer.Size = new System.Drawing.Size(120, 88);
+            this.lstAnswer.TabIndex = 8;
+            // 
+            // cboAnwer
+            // 
+            this.cboAnwer.FormattingEnabled = true;
+            this.cboAnwer.Items.AddRange(new object[] {
+            "咨询",
+            "服务",
+            "价格"});
+            this.cboAnwer.Location = new System.Drawing.Point(320, 271);
+            this.cboAnwer.Name = "cboAnwer";
+            this.cboAnwer.Size = new System.Drawing.Size(121, 20);
+            this.cboAnwer.TabIndex = 9;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(332, 326);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 10;
+            this.btnOK.Text = "提交";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(142, 39);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 381);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.cboAnwer);
+            this.Controls.Add(this.lstAnswer);
+            this.Controls.Add(this.chkAnswer4);
+            this.Controls.Add(this.chkAnswer3);
+            this.Controls.Add(this.chkAnswer2);
+            this.Controls.Add(this.chkAnswer1);
+            this.Controls.Add(this.rbtnAnswer2);
+            this.Controls.Add(this.rbtnAnswer1);
+            this.Controls.Add(this.lblQuestion1);
+            this.Controls.Add(this.lblQuestion5);
+            this.Controls.Add(this.lblQuestion4);
+            this.Controls.Add(this.lblQuestion3);
+            this.Controls.Add(this.lblQuestion2);
+            this.Name = "Form1";
+            this.Text = "客户问卷调查";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion2;
+        private System.Windows.Forms.Label lblQuestion3;
+        private System.Windows.Forms.Label lblQuestion4;
+        private System.Windows.Forms.Label lblQuestion5;
+        private System.Windows.Forms.Label lblQuestion1;
+        private System.Windows.Forms.RadioButton rbtnAnswer1;
+        private System.Windows.Forms.RadioButton rbtnAnswer2;
+        private System.Windows.Forms.CheckBox chkAnswer1;
+        private System.Windows.Forms.CheckBox chkAnswer2;
+        private System.Windows.Forms.CheckBox chkAnswer3;
+        private System.Windows.Forms.CheckBox chkAnswer4;
+        private System.Windows.Forms.ListBox lstAnswer;
+        private System.Windows.Forms.ComboBox cboAnwer;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ 项目3-客户问卷调查程序/简单版/Questionnaire/Questionnaire/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Questionnaire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string gender = "";
+            string impression = "客户对业务员印象:";//储存印象
+            if (rbtnAnswer1.Checked == true)
+            {
+                gender = "客户性别:" + rbtnAnswer1.Text;
+            }
+            if (rbtnAnswer2.Checked == true)
+            {
+                gender = "客户性别:" + rbtnAnswer2.Text;
+            }
+            if(chkAnswer1.Checked==true)
+            {
+                impression = impression + chkAnswer1.Text;
+            }
+            if (chkAnswer2.Checked == true)
+            {
+                impression = impression + chkAnswer2.Text;
+            }
+            if (chkAnswer3.Checked == true)
+            {
+                impression = impression + chkAnswer1.Text;
+            }
+            if (chkAnswer4.Checked == true)
+            {
+                impression = impression + chkAnswer4.Text;
+            }
+            if(txtName.Text=="")
+            {
+                MessageBox.Show("请输入姓名");
+            }
+            else
+            {
+                MessageBox.Show("客户姓名：" + txtName.Text.Trim() + "\r\n" + gender + "\r\n"+ impression + "\r\n" + "客户需要：" + lstAnswer.SelectedItem.ToString() + "\r\n" + "客户最满意：" + cboAnwer.Text, "问卷调查结果");
+            }
+
+        }
+    }
+}
