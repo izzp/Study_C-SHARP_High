ace0b49293c65180fd5831a51d053a014b7d56fb
雨心 <i@mezzp.com>
2019-04-03 00:25:48 +0800

    XML版本

---
+++ 项目3-客户问卷调查程序/xml版/Questionnaire/Questionnaire/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Questionnaire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Customers cs = new Customers();
+            cs.Name = txtName.Text;
+            if (rbtnAnswer1.Checked)
+                cs.Gender = rbtnAnswer1.Text;
+            if (rbtnAnswer2.Checked)
+                cs.Gender = rbtnAnswer2.Text;
+            if (chkAnswer1.Checked)
+                cs.Impression += chkAnswer1.Text + " ";
+            if (chkAnswer2.Checked)
+                cs.Impression += chkAnswer2.Text + " ";
+            if (chkAnswer3.Checked)
+                cs.Impression += chkAnswer3.Text + " ";
+            if (chkAnswer4.Checked)
+                cs.Impression += chkAnswer4.Text + " ";
+            cs.Satisfaction = cboAnwer.Text;
+            for (int i = 0; i < lstAnswer.SelectedItems.Count; i++)
+            {
+                cs.Service += lstAnswer.SelectedItems[i].ToString() + " ";
+            }
+            if (XMLOperate.Writer(cs))
+                MessageBox.Show("保存成功！");
+            else
+                MessageBox.Show("保存失败！");
+        }
+    }
+}
---
+++ 项目3-客户问卷调查程序/xml版/Questionnaire/Questionnaire/XMLOperate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Questionnaire
+{
+    class XMLOperate
+    {
+ static string path = @"../../customers.xml";
+        public static bool Writer(Customers cs)
+        {
+            bool result = false;
+            try
+            {
+                XElement customers = XElement.Load(path);
+                customers.Add("customers",
+                        new XElement("客户",
+                                new XElement("name", cs.Name),
+                                new XElement("gender", cs.Gender),
+                                new XElement("impression", cs.Impression),
+                                new XElement("satisfaction", cs.Satisfaction),
+                                new XElement("service", cs.Service)
+                            )
+                    );
+                customers.Save(path);
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
