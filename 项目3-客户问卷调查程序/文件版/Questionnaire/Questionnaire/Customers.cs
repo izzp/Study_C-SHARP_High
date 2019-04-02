b5d29ce043d5494cbd204f2e2bc39ffb018c28b5
雨心 <i@mezzp.com>
2019-04-02 23:56:13 +0800

    文件普通版本

---
+++ 项目3-客户问卷调查程序/文件版/Questionnaire/Questionnaire/Customers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCustomer
+{
+    class Customers
+    {
+        string name;//姓名
+        string gender;//性别
+        string impression;//印象
+        string satisfaction;//满意的方面
+        string service; //服务
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public string Impression
+        {
+            get { return impression; }
+            set { impression = value; }
+        }
+        public string Satisfaction
+        {
+            get { return satisfaction; }
+            set { satisfaction = value; }
+        }
+        public string Service
+        {
+            get { return service; }
+            set { service = value; }
+        }
+    }
+}
---
+++ 项目3-客户问卷调查程序/文件版/Questionnaire/Questionnaire/txtOperate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCustomer;
+
+namespace Questionnaire
+{
+    class txtOperate
+    {
+        static string path = @"../../customer.txt";
+        public static bool Writer(Customers cs)
+        {
+            bool result = false;
+            StreamWriter sw = null;
+            string line = cs.Name + '\t' + cs.Gender + '\t' + cs.Impression + '\t' + cs.Satisfaction + '\t' + cs.Service;
+            try
+            {
+                sw = new StreamWriter(path, true, Encoding.UTF8);
+                sw.WriteLine(line);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
